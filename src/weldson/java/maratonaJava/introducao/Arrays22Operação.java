9a24511ab7c6e4a5699b8ab3ace86e7dc0c8f033
Gabriel Borges <gabrielborgesw2@gmail.com>
2025-08-12 04:55:27 -0300

    array

--- src/weldson/java/maratonaJava/introducao/Arrays22Operação.java
+++ src/weldson/java/maratonaJava/introducao/Arrays22Operação.java
@@ -1,4 +1,17 @@
 package weldson.java.maratonaJava.introducao;
 
 public class Arrays22Operação {
+    public static void main(String[] args) {
+        String[] nomes = new String[5];
+        nomes[0] = "Weldson";
+        nomes[1] = "Welds";
+        nomes[2] = "Wel";
+        nomes[3] = "Weldso";
+        nomes[4] = "We";
+
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println("Nome na posição " + i + ": " + nomes[i]);
+        }
+
+    }
 }
